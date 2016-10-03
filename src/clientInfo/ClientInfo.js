61d49222695666928b0b023f48ee495f5941b6ff
Ildar Galeev <KeinAsylum@gmail.com>
2016-10-03 16:11:33 +0300

    FE-48: Removed ip address from tokenization request. Removed duplicate dependency

--- src/clientInfo/ClientInfo.js
+++ src/clientInfo/ClientInfo.js
@@ -1,18 +1,14 @@
-import Geoinfo from './Geoinfo';
 import Fingerprint from 'fingerprintjs2';
 
 export default class {
     constructor() {
         this.fingerprint = '';
-        this.ipAddress = '';
         new Fingerprint().get(result => this.fingerprint = result);
-        Geoinfo.get().then(result => this.ipAddress = result.ip);
     }
 
     getInfo() {
         return {
-            fingerprint: this.fingerprint,
-            ipAddress: this.ipAddress
+            fingerprint: this.fingerprint
         }
     }
 }
