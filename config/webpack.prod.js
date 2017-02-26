35c3952efdce9fc2a949fc3a9a70315dc0d39f7c
Ildar Galeev <KeinAsylum@gmail.com>
2017-02-26 22:20:17 +0300

    FE-219: Added webpack. Updated build image tag. Added init with cache.

---
+++ config/webpack.prod.js
@@ -0,0 +1,27 @@
+const webpack = require('webpack');
+const webpackMerge = require('webpack-merge');
+const commonConfig = require('./webpack.common.js');
+
+module.exports = webpackMerge(commonConfig, {
+    plugins: [
+        new webpack.LoaderOptionsPlugin({
+            minimize: true,
+            debug: false
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+                compress: {
+                    warnings: false,
+                    screw_ie8: true,
+                    conditionals: true,
+                    unused: true,
+                    comparisons: true,
+                    sequences: true,
+                    dead_code: true,
+                    evaluate: true,
+                    if_return: true,
+                    join_vars: true,
+                }
+            }
+        )
+    ]
+});
