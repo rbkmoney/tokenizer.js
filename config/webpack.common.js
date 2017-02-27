32fb3ca8b8442b6645b66937c6543add54f02ecd
Ildar Galeev <KeinAsylum@gmail.com>
2017-02-27 15:58:21 +0300

    FE-219: Webpack config fix.

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -16,11 +16,6 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.js/,
-                exclude: /node_modules/,
-                use: ['babel-loader']
-            },
-            {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 use: ['babel-loader', 'eslint-loader'],
