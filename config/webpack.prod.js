d0769c167f23d13bd17e90790000f36030e10213
Ildar Galeev <KeinAsylum@gmail.com>
2017-03-01 18:18:52 +0300

    FE-219: Style fix

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -9,19 +9,18 @@ module.exports = webpackMerge(commonConfig, {
             debug: false
         }),
         new webpack.optimize.UglifyJsPlugin({
-                compress: {
-                    warnings: false,
-                    screw_ie8: true,
-                    conditionals: true,
-                    unused: true,
-                    comparisons: true,
-                    sequences: true,
-                    dead_code: true,
-                    evaluate: true,
-                    if_return: true,
-                    join_vars: true,
-                }
+            compress: {
+                warnings: false,
+                screw_ie8: true,
+                conditionals: true,
+                unused: true,
+                comparisons: true,
+                sequences: true,
+                dead_code: true,
+                evaluate: true,
+                if_return: true,
+                join_vars: true,
             }
-        )
+        })
     ]
 });
