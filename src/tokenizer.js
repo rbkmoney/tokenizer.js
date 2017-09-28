9ee9fca23f96d8cc2b978771baa6d5e41ce5f216
Ildar Galeev <KeinAsylum@gmail.com>
2017-09-28 14:21:38 +0300

    FE-459: Added CAPI error handling.

--- src/tokenizer.js
+++ src/tokenizer.js
@@ -1,6 +1,7 @@
 import 'core-js/es6/promise';
 import RpcConsumer from './rpc/RpcConsumer';
 import getOrigin from './utils/getOrigin';
+import isFunction from './utils/isFunction';
 
 (function init() {
     const consumer = new RpcConsumer(getOrigin());
@@ -8,11 +9,17 @@ import getOrigin from './utils/getOrigin';
     this.Tokenizer = {
         setAccessToken: token => (accessToken = token),
         card: {
-            createToken: (cardData, success, error) => {
+            createToken: (cardData, successFn, errorFn) => {
                 if (accessToken) {
-                    consumer.createToken(accessToken, cardData, success, error);
+                    consumer.createToken(accessToken, cardData, (response) => {
+                        if (response.type === 'resolve' && isFunction(successFn)) {
+                            successFn(response.data);
+                        } else if (response.type === 'reject' && isFunction(errorFn)) {
+                            errorFn(response.data);
+                        }
+                    });
                 } else {
-                    error({message: 'Access token is required'});
+                    errorFn({message: 'Access token is required'});
                 }
             }
         }
--- src/tokenizers/CardTokenizer.js
+++ src/tokenizers/CardTokenizer.js
@@ -19,16 +19,25 @@ export default class CardTokenizer {
                     'Accept': 'application/json',
                     'Content-Type': 'application/json; charset=utf-8',
                     'X-Request-ID': generateGuid(),
-                    'Authorization': `Bearer ${accessToken}`,
+                    'Authorization': `Bearer ${accessToken}`
                 },
                 body: JSON.stringify(params)
             }).then((response) => {
                 if (response.status === 201) {
                     resolve(response.json());
-                } else {
-                    reject(response);
                 }
-            }).catch(() => reject({message: 'Invalid request to capi'}));
+                else {
+                    response.json()
+                        .then((error) => reject(error))
+                        .catch(() => {
+                            reject({
+                                url: response.url,
+                                status: response.status,
+                                statusText: response.statusText
+                            });
+                        });
+                }
+            }).catch((error) => reject(error));
         });
     }
 
